efb6846dd48ab41a6505fe816c4ae399de056b71
Khoa Nguyen <nguyenkh@cs.grinnell.edu>
2014-04-18 21:27:11 -0500

    format to Sam's standard

--- src/JSON/ArrayListToJSONString.java
+++ src/JSON/ArrayListToJSONString.java
@@ -4,38 +4,49 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 //Use the "toString()" method of this class to print out Java ArrayList in JSON format
-public class ArrayListToJSONString extends ArrayList {
-	// field
-	ArrayList arr;
+public class ArrayListToJSONString
+    extends
+      ArrayList
+{
+  // field
+  ArrayList arr;
 
-	// constructor
-	public ArrayListToJSONString(ArrayList inputArrayList) {
-		arr = inputArrayList;
-	}
+  // constructor
+  public ArrayListToJSONString(ArrayList inputArrayList)
+  {
+    arr = inputArrayList;
+  }// ArrayListToJSONString(ArrayList inputArrayList)
 
-	@Override
-	// return a JSON string for arr
-	public String toString() {
-		Iterator it = arr.iterator();
-		StringBuilder sb = new StringBuilder();
-		sb.append("[");
+  @Override
+  // return a JSON string for arr
+    public
+    String toString()
+  {
+    Iterator it = arr.iterator();
+    StringBuilder sb = new StringBuilder();
+    sb.append("[");
 
-		while (it.hasNext()) {
-			Object currentElement = it.next();
+    while (it.hasNext())
+      {
+        Object currentElement = it.next();
 
-			// add double quotation marks to elements that are strings
-			if (currentElement instanceof String) {
-				sb.append("\"" + currentElement + "\"");
-			} else {
-				sb.append(currentElement);
-			}//else
+        // add double quotation marks to elements that are strings
+        if (currentElement instanceof String)
+          {
+            sb.append("\"" + currentElement + "\"");
+          }// if
+        else
+          {
+            sb.append(currentElement);
+          }// else
 
-			if (it.hasNext()) {
-				sb.append(",");
-			}// if
-		}// while
+        if (it.hasNext())
+          {
+            sb.append(",");
+          }// if
+      }// while
 
-		sb.append("]");
-		return sb.toString();
-	}// toJSONString()
+    sb.append("]");
+    return sb.toString();
+  }// toJSONString()
 }// ArrayListToJSONString
--- src/JSON/HashtableToJSONString.java
+++ src/JSON/HashtableToJSONString.java
@@ -4,39 +4,47 @@ import java.util.Hashtable;
 import java.util.Iterator;
 
 //Use the "toString()" method of this class to print out Java Hashtable in JSON format
-public class HashtableToJSONString extends Hashtable {
+public class HashtableToJSONString
+    extends
+      Hashtable
+{
 
-	// field
-	Hashtable hb;
+  // field
+  Hashtable hb;
 
-	// constructor
-	HashtableToJSONString(Hashtable inputHB) {
-		hb = inputHB;
-	}//HashtableToJSONString(Hashtable inputHB)
+  // constructor
+  HashtableToJSONString(Hashtable inputHB)
+  {
+    hb = inputHB;
+  }// HashtableToJSONString(Hashtable inputHB)
 
-	@Override
-	// return a JSON string for hb
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append('{');
+  @Override
+  // return a JSON string for hb
+    public
+    String toString()
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append('{');
 
-		for (Iterator it = hb.keySet().iterator(); it.hasNext();) {
-			String key = (String) it.next();
-			sb.append("\"" + key + "\":");
+    for (Iterator it = hb.keySet().iterator(); it.hasNext();)
+      {
+        String key = (String) it.next();
+        sb.append("\"" + key + "\":");
 
-			Object value = (Object) hb.get(key);
-			if (value instanceof String) {
-				sb.append("\"" + value + "\"");
-			}//if 
-			else
-				sb.append(value);
-			
-			if (it.hasNext())
-			{
-				sb.append(",");
-			}//if
-		}//for
-		sb.append("}");
-		return sb.toString();
-	}//toString
-}//HashtableToJSONString
+        Object value = (Object) hb.get(key);
+        if (value instanceof String)
+          {
+            sb.append("\"" + value + "\"");
+          }// if
+        else
+          sb.append(value);
+
+        if (it.hasNext())
+          {
+            sb.append(",");
+          }// if
+      }// for
+    sb.append("}");
+    return sb.toString();
+  }// toString
+}// HashtableToJSONString
