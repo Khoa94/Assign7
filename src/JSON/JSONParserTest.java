d8c4e91dd853d0245e4343132fc26fc6bda5f73d
Khoa Nguyen <nguyenkh@cs.grinnell.edu>
2014-04-22 11:01:08 -0500

    fix code's format

--- src/JSON/JSONParserTest.java
+++ src/JSON/JSONParserTest.java
@@ -12,71 +12,72 @@ import org.junit.Test;
 public class JSONParserTest
 {
 
- @Test
- public void testJavaObjectToJSONString()
- {
-   //Convert Hashtable to string in JSON format
-   Hashtable hb1 = new Hashtable();
-   hb1.put("a", "1");
-   hb1.put("b", 2345);
-   hb1.put("c", "89");
-   HashtableToJSONString hb1JSON = new HashtableToJSONString(hb1);
-   assertEquals("{\"b\":2345,\"a\":\"1\",\"c\":\"89\"}", hb1JSON.toString());
+  @Test
+  public void testJavaObjectToJSONString()
+  {
+    // Convert Hashtable to string in JSON format
+    Hashtable hb1 = new Hashtable();
+    hb1.put("a", "1");
+    hb1.put("b", 2345);
+    hb1.put("c", "89");
+    HashtableToJSONString hb1JSON = new HashtableToJSONString(hb1);
+    assertEquals("{\"b\":2345,\"a\":\"1\",\"c\":\"89\"}", hb1JSON.toString());
 
-   //Convert ArrayList to string in JSON format
-   ArrayList ar1 = new ArrayList();
-   ar1.add("ab");
-   ar1.add(12);
-   ar1.add(hb1JSON);
-   ar1.add("c");
-   ArrayListToJSONString ar1JSON = new ArrayListToJSONString(ar1);
-   assertEquals("[\"ab\",12,{\"b\":2345,\"a\":\"1\",\"c\":\"89\"},\"c\"]", ar1JSON.toString());
-   
-   //A more complicated case
-   Hashtable hb2 = new Hashtable();
-   hb2.put("d", 123);
-   hb2.put("e", ar1JSON);
-   hb2.put("f", hb1JSON);
-   hb2.put("ghi", "xyz");
-   HashtableToJSONString hb2JSON = new HashtableToJSONString(hb2);
-   assertEquals("{\"f\":{\"b\":2345,\"a\":\"1\",\"c\":\"89\"},\"e\":[\"ab\",12,{\"b\":2345,\"a\":\"1\",\"c\":\"89\"},\"c\"],\"ghi\":\"xyz\",\"d\":123}", hb2JSON.toString());
- }//testJavaObjectToJSONString()
- 
- @Test
- public void testJSONStringToJavaObject()
- {
-	 //test 1
-   String str1 = "{\"firstName\":\"John\",\"lastName\":\"Doe\" }";
-   Hashtable hb1 = JSONParser.JSONObjectToHashtable(str1);
-   Hashtable hb1Expected = new Hashtable ();
-   hb1Expected.put("firstName", "John");
-   hb1Expected.put("lastName", "Doe");
-   assertTrue(hb1Expected.equals(hb1));
-   
-   //test 2
-   String str2 = "{\"id\":32,\"ugly\":[\"a\",{\"hello\":21}]}";
-   Hashtable hb2 = JSONParser.JSONObjectToHashtable(str2);
-   Hashtable hb2Expected = new Hashtable();
-   hb2Expected.put("id", 32);
-   
-   ArrayList ar = new ArrayList();
-   ar.add("a");
-   Hashtable temp = new Hashtable();
-   temp.put("hello", 21);
-   ar.add(temp);
+    // Convert ArrayList to string in JSON format
+    ArrayList ar1 = new ArrayList();
+    ar1.add("ab");
+    ar1.add(12);
+    ar1.add(hb1JSON);
+    ar1.add("c");
+    ArrayListToJSONString ar1JSON = new ArrayListToJSONString(ar1);
+    assertEquals("[\"ab\",12,{\"b\":2345,\"a\":\"1\",\"c\":\"89\"},\"c\"]",
+                 ar1JSON.toString());
 
-   
-   hb2Expected.put("ugly", ar);
-   
-   System.out.println(hb2Expected.toString());
-   System.out.println(hb2.toString());
-   assertTrue(hb2Expected.equals(hb2));
-   
-   //test 3
-   ArrayList ALTester1= new ArrayList();
-	ALTester1.add(1);
-	ALTester1.add(2);
-	assertEquals(JSONParser.JSONObjectToArrayList ("[1,2]"), ALTester1);
+    // A more complicated case
+    Hashtable hb2 = new Hashtable();
+    hb2.put("d", 123);
+    hb2.put("e", ar1JSON);
+    hb2.put("f", hb1JSON);
+    hb2.put("ghi", "xyz");
+    HashtableToJSONString hb2JSON = new HashtableToJSONString(hb2);
+    assertEquals("{\"f\":{\"b\":2345,\"a\":\"1\",\"c\":\"89\"},\"e\":[\"ab\",12,{\"b\":2345,\"a\":\"1\",\"c\":\"89\"},\"c\"],\"ghi\":\"xyz\",\"d\":123}",
+                 hb2JSON.toString());
+  }// testJavaObjectToJSONString()
 
- }//testJSONStringToJavaObject()
-}//JSONParserTest
+  @Test
+  public void testJSONStringToJavaObject()
+  {
+    // test 1
+    String str1 = "{\"firstName\":\"John\",\"lastName\":\"Doe\" }";
+    Hashtable hb1 = JSONParser.JSONObjectToHashtable(str1);
+    Hashtable hb1Expected = new Hashtable();
+    hb1Expected.put("firstName", "John");
+    hb1Expected.put("lastName", "Doe");
+    assertTrue(hb1Expected.equals(hb1));
+
+    // test 2
+    String str2 = "{\"id\":32,\"ugly\":[\"a\",{\"hello\":21}]}";
+    Hashtable hb2 = JSONParser.JSONObjectToHashtable(str2);
+    Hashtable hb2Expected = new Hashtable();
+    hb2Expected.put("id", 32);
+
+    ArrayList ar = new ArrayList();
+    ar.add("a");
+    Hashtable temp = new Hashtable();
+    temp.put("hello", 21);
+    ar.add(temp);
+
+    hb2Expected.put("ugly", ar);
+
+    System.out.println(hb2Expected.toString());
+    System.out.println(hb2.toString());
+    assertTrue(hb2Expected.equals(hb2));
+
+    // test 3
+    ArrayList ALTester1 = new ArrayList();
+    ALTester1.add(1);
+    ALTester1.add(2);
+    assertEquals(JSONParser.JSONObjectToArrayList("[1,2]"), ALTester1);
+
+  }// testJSONStringToJavaObject()
+}// JSONParserTest
